0388cae8846dead54d120ef9ddf4131299d1d039
Robert Marianski <rob@marianski.com>
2014-10-08 15:59:23 -0400

    Fill out setup.py metadata

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.0'
+version = '0.0.1dev'
 
 setup(name='tilequeue',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      description="Queue operations to manage the processes surrounding tile "
+                  "rendering.",
+      long_description=open('README.md').read(),
+      classifiers=[
+          # strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Internet :: WWW/HTTP :: Site Management',
+          'Topic :: Utilities',
+      ],
+      keywords='aws queue s3 sqs tile map',
+      author='Robert Marianski, Mapzen',
+      author_email='rob@mapzen.com',
+      url='https://github.com/mapzen/tilequeue',
+      license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
@@ -25,7 +37,7 @@ setup(name='tilequeue',
       ],
       test_suite='tests',
       entry_points=dict(
-          console_scripts = [
+          console_scripts=[
               'tilequeue = tilequeue.command:tilequeue_main',
           ]
       )
