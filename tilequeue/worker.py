c006b7cfb9beb830d2e8d3be1b8c11ed4a7e1203
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-12-16 14:05:16 -0500

    Revert "capturing errors so the workers will not die"
    This reverts commit f4b200f91f26d6d8d120533be66e39a3bf670e81.

--- tilequeue/worker.py
+++ tilequeue/worker.py
@@ -1,7 +1,6 @@
 from tilequeue.tile import serialize_coord
 import time
 from tilequeue.utils import trap_signal
-from psycopg2.extensions import TransactionRollbackError
 
 
 class Worker(object):
@@ -25,19 +24,14 @@ class Worker(object):
         while True:
             msgs = self.queue.read(max_to_read=max_to_read)
             for msg in msgs:
-                try:
-                    start_time = time.time()
-                    coord = msg.coord
-                    coord_str = serialize_coord(coord)
-                    self._log('processing %s ...' % coord_str)
-                    self.job_creator.process_jobs_for_coord(msg.coord)
-                    self.queue.job_done(msg.message_handle)
-                    total_time = time.time() - start_time
-                    self._log('processing %s ... done took %s (seconds)'
-                              % (coord_str, total_time))
-                except TransactionRollbackError:
-                    self._log('TransactionRollbackError continuing...')
-                except Exception as e:
-                    self._log(e)
+                start_time = time.time()
+                coord = msg.coord
+                coord_str = serialize_coord(coord)
+                self._log('processing %s ...' % coord_str)
+                self.job_creator.process_jobs_for_coord(msg.coord)
+                self.queue.job_done(msg.message_handle)
+                total_time = time.time() - start_time
+                self._log('processing %s ... done took %s (seconds)'
+                          % (coord_str, total_time))
             if not self.daemonized:
                 break
