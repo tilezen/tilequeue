20c1a42706a519b8caf8b0a961e1fb7d39b9d24c
Robert Marianski <rob@marianski.com>
2015-01-13 13:13:57 -0500

    Create custom threaded connection pool
    This pool is designed to be a simpler implementation, and attempts to be
    much safer when returning connections back to the pool.

--- tilequeue/postgresql.py
+++ tilequeue/postgresql.py
@@ -1,5 +1,7 @@
+from itertools import chain
 from itertools import cycle
 import psycopg2
+import threading
 
 
 class RoundRobinConnectionFactory(object):
@@ -36,3 +38,79 @@ class NoPoolingConnectionPool(object):
 
     def closeall(self):
         pass
+
+
+class ThreadedConnectionPool(object):
+
+    # Custom version of a threaded connection pool. This is a simpler
+    # implementation than what the postgresql connection pool does. In
+    # particular, this pool attempts to be much safer when returning
+    # connections back to the pool.
+
+    def __init__(self, minconn, maxconn, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.maxconn = maxconn
+        self.lock = threading.Lock()
+        self.not_in_use = {}
+        self.in_use = {}
+
+        for i in xrange(minconn):
+            conn = self._make_conn()
+            self.not_in_use[id(conn)] = conn
+
+    def _make_conn(self):
+        return psycopg2.connect(*self.args, **self.kwargs)
+
+    def getconn(self, key=None):
+        assert key is None, 'Keys not supported'
+
+        self.lock.acquire()
+        try:
+            if self.not_in_use:
+                conn_id, conn = self.not_in_use.popitem()
+                self.in_use[conn_id] = conn
+                return conn
+            else:
+                if len(self.in_use) == self.maxconn:
+                    raise RuntimeError(
+                        'Maximum number of connections created: %d' %
+                        self.maxconn)
+                conn = self._make_conn()
+                self.in_use[id(conn)] = conn
+                return conn
+        finally:
+            self.lock.release()
+
+    def putconn(self, conn, key=None, close=False):
+        assert key is None, 'Keys not supported'
+
+        self.lock.acquire()
+        try:
+            conn_id = id(conn)
+            if conn_id not in self.in_use:
+                raise ValueError('Connection not part of pool')
+            del self.in_use[conn_id]
+            if close:
+                try:
+                    conn.close()
+                except:
+                    pass
+            if not conn.closed:
+                self.not_in_use[conn_id] = conn
+        finally:
+            self.lock.release()
+
+    def closeall(self):
+        self.lock.acquire()
+        try:
+            for conn_id, conn in chain(self.not_in_use.iteritems(),
+                                       self.in_use.iteritems()):
+                try:
+                    conn.close()
+                except:
+                    pass
+            self.not_in_use.clear()
+            self.in_use.clear()
+        finally:
+            self.lock.release()
