39eb5471f052968c8272b523dd4eefd2ed997de0
Robert Marianski <rob@marianski.com>
2016-05-04 15:28:10 -0400

    Update geojson encoding
    * More efficient encoding of multiple layers
    * Improved precision handling
      - on invalid geometries, use full precision
      - can now use less precision if not needed

--- tilequeue/format/__init__.py
+++ tilequeue/format/__init__.py
@@ -1,7 +1,7 @@
 from cStringIO import StringIO
 from json import loads
-from TileStache.Goodies.VecTiles.geojson import encode as json_encode
-from TileStache.Goodies.VecTiles.geojson import merge as json_merge
+from tilequeue.format.geojson import encode_multiple_layers as json_encode_multiple_layers  # noqa
+from tilequeue.format.geojson import encode_single_layer as json_encode_single_layer  # noqa
 from TileStache.Goodies.VecTiles.mvt import merge as mvt_merge
 from TileStache.Goodies.VecTiles.oscimap import merge as vtm_merge
 from TileStache.Goodies.VecTiles.topojson import encode as topojson_encode
@@ -38,27 +38,21 @@ class OutputFormat(object):
 
 # consistent facade around all tilestache formatters that we use
 def format_json(fp, feature_layers, coord, bounds_merc, bounds_wgs84):
-    # TODO a lot of serializing/deserializing can be reduced here
-    # this is a faithful port for how it's done in tilestache now
     if len(feature_layers) == 1:
-        json_encode(fp, feature_layers[0]['features'], coord.zoom)
+        json_encode_single_layer(fp, feature_layers[0]['features'], coord.zoom)
         return
-    names = []
-    layers = []
-    for feature_layer in feature_layers:
-        names.append(feature_layer['name'])
-        out = StringIO()
-        json_encode(out, feature_layer['features'], coord.zoom)
-        # out now contains a json serialized result
-        # now we deserialize it, so that it can be combined with the
-        # merge function
-        deserialized_features = loads(out.getvalue())
-        layers.append(deserialized_features)
-    json_merge(fp, names, layers, None, coord)
+    else:
+        features_by_layer = {}
+        for feature_layer in feature_layers:
+            layer_name = feature_layer['name']
+            features = feature_layer['features']
+            features_by_layer[layer_name] = features
+        json_encode_multiple_layers(fp, features_by_layer, coord.zoom)
 
 
 def format_topojson(fp, feature_layers, coord, bounds_merc, bounds_wgs84):
-    # TODO ditto on the serialization as in format_json
+    # TODO a lot of serializing/deserializing can be reduced here
+    # this is a faithful port for how it's done in tilestache now
     if len(feature_layers) == 1:
         topojson_encode(fp, feature_layers[0]['features'], bounds_wgs84)
         return
---
+++ tilequeue/format/geojson.py
@@ -0,0 +1,79 @@
+from math import ceil
+from math import log
+import json
+import shapely.geometry
+import shapely.ops
+import shapely.wkb
+
+precisions = [int(ceil(log(1 << zoom + 8+2) / log(10)) - 2)
+              for zoom in range(17)]
+# at z16, need to be more precise for metatiling
+precisions[16] = 8
+
+
+class JsonFeatureCreator(object):
+
+    def __init__(self, precision=None):
+        self.precision = precision
+
+    def _trim_precision(self, x, y, z=None):
+        return round(x, self.precision), round(y, self.precision)
+
+    def __call__(self, feature):
+        assert len(feature) == 3
+        wkb_or_shape, props, fid = feature
+        if isinstance(wkb_or_shape, shapely.geometry.base.BaseGeometry):
+            shape = wkb_or_shape
+        else:
+            shape = shapely.wkb.loads(wkb_or_shape)
+
+        if self.precision:
+            truncated_precision_shape = shapely.ops.transform(
+                self._trim_precision, shape)
+            if truncated_precision_shape.is_valid:
+                shape = truncated_precision_shape
+
+        geometry = shape.__geo_interface__
+        result = dict(type='Feature', properties=props, geometry=geometry)
+        if fid is not None:
+            result['id'] = fid
+        return result
+
+
+def create_layer_feature_collection(features, precision):
+    create_json_feature = JsonFeatureCreator(precision)
+    fs = map(create_json_feature, features)
+    feature_collection = dict(type='FeatureCollection', features=fs)
+    return feature_collection
+
+
+def precision_for_zoom(zoom):
+    precision_idx = zoom if 0 <= zoom < len(precisions) else -1
+    precision = precisions[precision_idx]
+    return precision
+
+
+def encode_single_layer(out, features, zoom):
+    """
+    Encode a list of (WKB|shapely, property dict, id) features into a
+    GeoJSON stream.
+
+    If no id is available, pass in None
+
+    Geometries in the features list are assumed to be lon, lats.
+    """
+    precision = precision_for_zoom(zoom)
+    fs = create_layer_feature_collection(features, precision)
+    json.dump(fs, out)
+
+
+def encode_multiple_layers(out, features_by_layer, zoom):
+    """
+    features_by_layer should be a dict: layer_name -> feature tuples
+    """
+    precision = precision_for_zoom(zoom)
+    geojson = {}
+    for layer_name, features in features_by_layer.items():
+        fs = create_layer_feature_collection(features, precision)
+        geojson[layer_name] = fs
+    json.dump(geojson, out)
