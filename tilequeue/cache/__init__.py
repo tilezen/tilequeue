1e26d92ad8334eca3514489517dc3e9066b10493
Ian Dees <ian.dees@gmail.com>
2017-04-28 14:59:02 -0400

    Remove stuff related to redis_cache_index

--- tilequeue/cache/__init__.py
+++
@@ -1,4 +0,0 @@
-from redis_cache_index import RedisCacheIndex
-from stub_index import StubIndex
-
-__all__ = [RedisCacheIndex, StubIndex]
--- tilequeue/toi/__init__.py
+++ tilequeue/toi/__init__.py
@@ -6,3 +6,12 @@ from .file import (
     load_set_from_gzipped_fp,
 )
 from .s3 import S3TilesOfInterestSet
+
+__all__ = [
+    FileTilesOfInterestSet,
+    S3TilesOfInterestSet,
+    save_set_to_fp,
+    load_set_from_fp,
+    save_set_to_gzipped_fp,
+    load_set_from_gzipped_fp,
+]
