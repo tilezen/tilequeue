2a71d6855743226a15f3df38edcf95bb2aa44a08
Robert Marianski <rob@marianski.com>
2017-08-08 16:11:31 -0400

    Expose fixture data fetcher under a different name

--- tilequeue/query/__init__.py
+++ tilequeue/query/__init__.py
@@ -2,8 +2,11 @@ from tilequeue.query.fixture import make_data_fetcher
 from tilequeue.query.pool import DBConnectionPool
 from tilequeue.query.postgres import make_db_data_fetcher
 
+# expose the fixture data fetcher under a different name
+make_fixture_data_fetcher = make_data_fetcher
+
 __all__ = [
     'DBConnectionPool',
-    'make_data_fetcher',
     'make_db_data_fetcher',
+    'make_fixture_data_fetcher',
 ]
