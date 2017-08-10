5d193dbc305e8362eaab7d1eacc53866d81b05dc
Matt Amos <matt.amos@mapzen.com>
2017-08-10 17:39:31 +0100

    Add label placement generation for fixture data.

--- tilequeue/query/fixture.py
+++ tilequeue/query/fixture.py
@@ -8,14 +8,16 @@ LayerInfo = namedtuple('LayerInfo', 'min_zoom_fn props_fn')
 
 class DataFetcher(object):
 
-    def __init__(self, layers, rows):
+    def __init__(self, layers, rows, label_placement_layers):
         """
         Expect layers to be a dict of layer name to LayerInfo. Expect rows to
-        be a list of (fid, shape, properties).
+        be a list of (fid, shape, properties). Label placement layers should
+        be a set of layer names for which to generate label placement points.
         """
 
         self.layers = layers
         self.rows = rows
+        self.label_placement_layers = label_placement_layers
 
     def __call__(self, zoom, unpadded_bounds):
         read_rows = []
@@ -29,6 +31,9 @@ class DataFetcher(object):
             # place for assembing the read row as if from postgres
             read_row = {}
 
+            # whether to generate a label placement centroid
+            generate_label_placement = False
+
             for layer_name, info in self.layers.items():
                 meta = meta_for_properties(props)
                 min_zoom = info.min_zoom_fn(shape, props, fid, meta)
@@ -36,6 +41,11 @@ class DataFetcher(object):
                 # reject anything which isn't in the current zoom range
                 if min_zoom is None or zoom < min_zoom:
                     continue
+
+                # if the feature exists in any label placement layer, then we
+                # should consider generating a centroid (if it's a polygon)
+                if layer_name in self.label_placement_layers:
+                    generate_label_placement = True
 
                 layer_props = props.copy()
                 layer_props['min_zoom'] = min_zoom
@@ -47,10 +57,14 @@ class DataFetcher(object):
             if read_row:
                 read_row['__id__'] = fid
                 read_row['__geometry__'] = bytes(shape.wkb)
+                if shape.geom_type in ('Polygon', 'MultiPolygon') and \
+                   generate_label_placement:
+                    read_row['__label__'] = bytes(
+                        shape.representative_point().wkb)
                 read_rows.append(read_row)
 
         return read_rows
 
 
-def make_data_fetcher(layers, rows):
-    return DataFetcher(layers, rows)
+def make_fixture_data_fetcher(layers, rows, label_placement_layers=set()):
+    return DataFetcher(layers, rows, label_placement_layers)
