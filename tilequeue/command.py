72d173b0fa83e8f483cf8f4cf1af0af77e409cd5
Matt Amos <matt.amos@mapzen.com>
2017-11-21 17:30:56 +0000

    Make metatile start zoom configurable.

--- tilequeue/command.py
+++ tilequeue/command.py
@@ -688,7 +688,8 @@ def tilequeue_process(cfg, peripherals):
 
     data_fetch = DataFetch(
         feature_fetcher, tile_input_queue, sql_data_fetch_queue, io_pool,
-        tile_proc_logger, stats_handler, cfg.metatile_zoom, cfg.max_zoom)
+        tile_proc_logger, stats_handler, cfg.metatile_zoom, cfg.max_zoom,
+        cfg.metatile_start_zoom)
 
     data_processor = ProcessAndFormatData(
         post_process_data, formats, sql_data_fetch_queue, processor_queue,
