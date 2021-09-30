dfb278b323b0fedbd0125863333e3f01ab128580
Peiti Li <pli2@snapchat.com>
2021-09-30 12:50:05 -0700

    add arg to low zoom to avoid exception

--- tilequeue/command.py
+++ tilequeue/command.py
@@ -2567,6 +2567,9 @@ def tilequeue_main(argv_args=None):
                            help='optional string of db password e.g. `VHcDuAS0SYx2tlgTvtbuCXwlvO4pAtiGCuScJFjq7wersdfqwer`')  # noqa
     subparser.add_argument('--store_name', required=False,
                            help='optional string of a list of tile store names e.g. `["my-meta-tiles-us-east-1"]`')  # noqa
+    subparser.add_argument('--rawr_store_name', required=False,
+                           help='optional string of rawr tile store '
+                                'names e.g. `"my-rawr-tiles-us-east-1"`')
     subparser.add_argument('--store_date_prefix', required=False,
                            help='optional string of store bucket date prefix e.g. `20210426`')  # noqa
     subparser.add_argument('--batch_check_metafile_exists', required=False,
