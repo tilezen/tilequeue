40ba90a7da6295257f0ed64a23b595c43eb81fa8
Kevin Kreiser <kevinkreiser@gmail.com>
2022-07-28 11:59:57 -0400

    leave in testing commented out for future testers

--- tilequeue/command.py
+++ tilequeue/command.py
@@ -2177,6 +2177,8 @@ def tilequeue_meta_tile(cfg, args):
         # each coord here is the unit of work now
         pyramid_coords = [job_coord]
         pyramid_coords.extend(coord_children_range(job_coord, zoom_stop))
+        # for brevity of testing it is sometimes convenient to reduce to a single child coord
+        #pyramid_coords = [Coordinate(zoom=13, column=2411, row=3080)]
         coord_data = [dict(coord=x) for x in pyramid_coords]
 
         try:
